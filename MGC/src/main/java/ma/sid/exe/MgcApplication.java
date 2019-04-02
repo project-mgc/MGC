04bb63631a4411d17ca4cdcfcd4e95e7291d90de
mloua <mloua@PC_MOHAMED>
2019-04-02 17:29:05 +0100

    First commmit

---
+++ MGC/src/main/java/ma/sid/exe/MgcApplication.java
@@ -0,0 +1,13 @@
+package ma.sid.exe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgcApplication.class, args);
+	}
+
+}
---
+++ MGC/src/test/java/ma/sid/exe/MgcApplicationTests.java
@@ -0,0 +1,16 @@
+package ma.sid.exe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MgcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
